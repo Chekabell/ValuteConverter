2529cf4805870860784e5210406727db13e55d4a
Chekabell <nikita.toporischev@mail.ru>
2025-01-13 16:00:25 +0500

    MVP

---
+++ ValuteConverter/RatesJson.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ValuteConverter;
+
+public class RatesJson
+{
+    public DateTime Date { get; set; }
+    public DateTime PreviousDate { get; set; }
+    public string PreviousURL { get; set; }
+    public DateTime Timestamp { get; set; }
+
+    [JsonPropertyName("Valute")]
+    public Dictionary<string, Valute> Rates { get; set; } = new();
+}
---
+++ ValuteConverter/Valute.cs
@@ -0,0 +1,9 @@
+﻿namespace ValuteConverter;
+
+public class Valute
+{
+    public string CharCode { get; set; }
+    public int Nominal { get; set; }
+    public string Name { get; set; }
+    public double Value { get; set; }
+}
